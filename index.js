84e6beb918a55163ff38c70e82fe5bc004cc1116
Adi-204 <adiboghawala@gmail.com>
2023-08-19 09:44:33 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var randonNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "./images/dice"+randonNumber1+".png";
+
+document.querySelector(".img1").setAttribute("src",randomDiceImage);
+
+
+var randonNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "./images/dice"+randonNumber2+".png";
+
+document.querySelector(".img2").setAttribute("src",randomDiceImage2);
+
+if(randonNumber1>randonNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if(randonNumber2>randonNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
