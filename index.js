f3c45e8f33be90cec8e16210e225e19884c2447c
Adi-204 <adiboghawala@gmail.com>
2023-08-25 19:01:08 +0530

    Button activated

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-let randonNumber1 = Math.floor(Math.random()*6)+1;
+let btn1 = document.getElementsByTagName("button")[0];
+btn1.addEventListener('click',function(){
+    let randonNumber1 = Math.floor(Math.random()*6)+1;
+    let randomDiceImage = "./images/dice"+randonNumber1+".png";
+    document.querySelector(".img1").setAttribute("src",randomDiceImage); let randonNumber2 = Math.floor(Math.random()*6)+1;
+    let randomDiceImage2 = "./images/dice"+randonNumber2+".png";
+    document.querySelector(".img2").setAttribute("src",randomDiceImage2);
+    if(randonNumber1>randonNumber2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    }
+    else if(randonNumber2>randonNumber1){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+})
 
-let randomDiceImage = "./images/dice"+randonNumber1+".png";
-
-document.querySelector(".img1").setAttribute("src",randomDiceImage);
-
-
-let randonNumber2 = Math.floor(Math.random()*6)+1;
-
-let randomDiceImage2 = "./images/dice"+randonNumber2+".png";
-
-document.querySelector(".img2").setAttribute("src",randomDiceImage2);
-
-if(randonNumber1>randonNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
-}
-else if(randonNumber2>randonNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
-}
-else{
-    document.querySelector("h1").innerHTML = "Draw!";
-}
 
